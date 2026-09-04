02f9c82ebfd98fc7d78827f1da8fd01486efbe45
softpoptecnologia <softpop.tecnologia@gmail.com>
2026-09-04 19:33:55 -0300

    atualização

--- app.py
+++ app.py
@@ -3,17 +3,16 @@ import sqlite3
 app = Flask(__name__)
 
 
+
 @app.route("/")
 def index():
-    conexao = sqlite3.connect("banco.db")
+    return render_template("admin/index.html")
 
-    categoria = conexao.execute( ' SELECT * FROM CATEGORIAS' )
+@app.route("/listar_categorias")
+def listarCategorias():
+    return render_template('admin/listar_categoria.html')
 
-    return render_template("admin/index.html", categoria = categoria)
 
-@app.route("/produtos")
-def produtos():
-    return "<h1> Cadastre seu produto</h1>"
 
 
 app.run(debug=True )
